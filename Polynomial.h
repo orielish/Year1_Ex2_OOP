64acf791001ba98e64cc2fddb284fdf9e33ca95d
Megidish <105859276+Megidish@users.noreply.github.com>
2022-08-21 21:49:25 +0300

    Add files via upload
    blqa blqa

---
+++ Polynomial.h
@@ -0,0 +1,43 @@
+#pragma once
+class Polynomial
+{
+	
+private:
+	const static int size = 20;   // ���� ���� �������
+	double _a[size];              // ���� �������
+	int _degree;                  // ���� ������ ������� �����
+	static int MaxDegree;         // Global highest Real Degree 
+
+
+public:
+
+	//constractures
+	Polynomial();
+	Polynomial(int);
+	Polynomial(double[], int );
+	Polynomial(const Polynomial& );
+	void copyP(Polynomial&);
+
+	//operators
+	Polynomial operator+(const Polynomial& other);
+	Polynomial operator-(const Polynomial& other);
+	//Polynomial operator*(const Polynomial& other);
+	void operator=(const Polynomial& other);
+
+	//Gets
+	int getDegree(bool) const;
+	double getCoeff(int) const;
+	static int getMaxDegree();
+
+	//Sets
+	void setCoeff(int, double);
+	void setDegree(int);
+	void print() const;
+	//friend ostream& operator << (ostream& ,const Polynomial&);
+
+
+
+
+
+};
+
